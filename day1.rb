15e31c314c5e4abc5da886229dfab7f8149e25c5
Mike Skalnik <mike.skalnik@gmail.com>
2017-12-01 11:15:24 -0800

    S T A L E :bread:

--- day1.rb
+++ day1.rb
@@ -24,19 +24,6 @@ class Day1
     sum
   end
 
-  def sum_captcha(captcha)
-    sum = 0
-    captcha_chars = captcha.chars
-    captcha_chars.each_with_index do |digit, index|
-      next_index = (index + 1) % captcha_chars.length
-      next_digit = captcha_chars[next_index]
-
-      sum += digit.to_i if digit == next_digit
-    end
-
-    sum
-  end
-
   def index_part_1(current_index, length)
     (current_index + 1) % length
   end
