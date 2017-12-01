c4eea8874fefbc47ee230dc3dffb463321e001fe
Mike Skalnik <mike.skalnik@gmail.com>
2017-12-01 11:12:05 -0800

    Combine into one file :closed_umbrella:

--- day1.rb
+++ day1.rb
@@ -1,16 +1,50 @@
 #!/usr/bin/env ruby
 
-def sum_captcha(captcha)
-  sum = 0
-  captcha_chars = captcha.chars
-  captcha_chars.each_with_index do |digit, index|
-    next_index = (index + 1) % captcha_chars.length
-    next_digit = captcha_chars[next_index]
-
-    sum += digit.to_i if digit == next_digit
+class Day1
+  def self.run(input)
+    puts "Part 1: #{Day1.new.run(input, part: 1)}"
+    puts "Part 2: #{Day1.new.run(input, part: 2)}"
   end
 
-  sum
+  def run(input, part: 1)
+    sum = 0
+    captcha_chars = input.chars
+    captcha_chars.each_with_index do |digit, index|
+      next_index = -1
+      if part == 1
+        next_index = index_part_1(index, captcha_chars.length)
+      else
+        next_index = index_part_2(index, captcha_chars.length)
+      end
+      next_digit = captcha_chars[next_index]
+
+      sum += digit.to_i if digit == next_digit
+    end
+
+    sum
+  end
+
+  def sum_captcha(captcha)
+    sum = 0
+    captcha_chars = captcha.chars
+    captcha_chars.each_with_index do |digit, index|
+      next_index = (index + 1) % captcha_chars.length
+      next_digit = captcha_chars[next_index]
+
+      sum += digit.to_i if digit == next_digit
+    end
+
+    sum
+  end
+
+  def index_part_1(current_index, length)
+    (current_index + 1) % length
+  end
+
+  def index_part_2(current_index, length)
+    distance_away = current_index + (length / 2)
+    distance_away % length
+  end
 end
 
-puts sum_captcha ARGV[0]
+Day1.run ARGV[0]
