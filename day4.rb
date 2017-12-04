7eaa3393aa3d0bb8d6363afad897e8294a7c5137
Mike Skalnik <mike.skalnik@gmail.com>
2017-12-03 23:15:32 -0800

    :bath:

--- day4.rb
+++ day4.rb
@@ -2,20 +2,40 @@
 
 class Day4
   def self.run(input_file)
-    correct_passphrases = 0
-    File.open(input_file).each_line do |passphrase|
-      correct_passphrases += 1 if valid_passphrase(passphrase)
-    end
-    puts correct_passphrases
+    passphrases = []
+    File.open(input_file).each_line { |line| passphrases << line.chomp }
+    passphrases.compact!
+
+    puts "Part 1: #{Day4.new(passphrases).run(part: 1)}"
+    puts "Part 2: #{Day4.new(passphrases).run(part: 2)}"
   end
 
-  def self.valid_passphrase(passphrase)
-    passwords = passphrase.split(' ')
-    #passwords.uniq == passwords
+  def initialize(passphrases)
+    @passphrases = passphrases
+  end
 
-    passwords.map! { |word| word.chars.sort.join('') }
+  def run(part:)
+    @passphrases.inject(0) do |sum, phrase|
+      if part == 1
+        sum += 1 if valid_passphrase_1(phrase)
+      else
+        sum += 1 if valid_passphrase_2(phrase)
+      end
 
-    passwords.uniq.size == passphrase.split(' ').size
+      sum
+    end
+  end
+
+  def valid_passphrase_1(phrase)
+    words = phrase.split(' ')
+    words == words.uniq
+  end
+
+  def valid_passphrase_2(phrase)
+    words = phrase.split(' ')
+    valid_word_count = words.map { |word| word.chars.sort.join('') }.uniq.size
+
+    valid_word_count == words.size
   end
 end
 
