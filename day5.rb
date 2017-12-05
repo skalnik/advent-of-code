3f52b13ec4d8249bc7d796ed4077a5bed4c97b75
Mike Skalnik <mike.skalnik@gmail.com>
2017-12-04 22:23:30 -0800

    :bathtub:

--- day5.rb
+++ day5.rb
@@ -5,12 +5,24 @@ class Day5
     maze = []
     File.open(input_file).each_line { |offset| maze << offset.to_i }
 
+    puts "Part 1: #{Day5.new(maze).run(part: 1)}"
+    puts "Part 2: #{Day5.new(maze).run(part: 2)}"
+  end
+
+  def initialize(maze)
+    @maze = maze
+  end
+
+  def run(part:)
+    maze = @maze.clone
+
     out_of_maze = false
     current_index = 0
     step_count = 0
     while !out_of_maze do
       next_index = current_index + maze[current_index]
-      if maze[current_index] >= 3
+
+      if maze[current_index] >= 3 && part != 1
         maze[current_index] -= 1
       else
         maze[current_index] += 1
@@ -22,7 +34,7 @@ class Day5
       current_index = next_index
     end
 
-    puts step_count
+    step_count
   end
 end
 
