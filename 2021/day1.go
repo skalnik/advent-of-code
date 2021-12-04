e7e31e4dc1744f97ea68c07533893eb9e909f20a
Mike Skalnik <mike@skalnik.com>
2021-12-03 20:31:37 -0800

    🐁

--- 2021/day1.go
+++ 2021/day1.go
@@ -3,9 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -54,5 +56,43 @@ func part1() {
 }
 
 func part2() {
+	contents, err := ioutil.ReadFile("input/day1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	lines := strings.Split(string(contents), "\n")
+	var depths []int
+	var a, b, c, sums []int
+
+	for i := 0; i < len(lines); i++ {
+		if lines[i] == "" {
+			break
+		}
+		depth, err := strconv.Atoi(lines[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		depths = append(depths, depth)
+	}
+
+	a = depths[2:]
+	b = depths[1 : len(depths)-1]
+	c = depths[:len(depths)-2]
+
+	for i := 0; i < len(a); i++ {
+		sums = append(sums, a[i]+b[i]+c[i])
+	}
+
+	increases := 0
+	previous := sums[0]
+	for i := 1; i < len(sums); i++ {
+		if sums[i] > previous {
+			increases++
+		}
+		previous = sums[i]
+	}
+
+	fmt.Println(increases)
 }
