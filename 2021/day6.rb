58ea5022f48ad1f8ab829dee9a25968a30648375
Mike Skalnik <mike@skalnik.com>
2021-12-06 09:27:03 -0800

    🕡️⏩️

--- 2021/day6.rb
+++ 2021/day6.rb
@@ -26,45 +26,63 @@ class Day6
   end
 
   def part_two
+    school = nil
+    File.open(@filename) do |file|
+      ages = file.readlines(chomp: true)[0].split(",").map(&:to_i)
+      fish = ages.map { |age| Fish.new(age) }
+      school = School.new(fish)
+    end
+
+    256.times do
+      school.step
+    end
+
+    school.size
   end
 end
 
 class School
   attr_reader :fish
   def initialize(fish)
-    @fish = fish
+    @fish = Array.new(9, 0)
+    fish.each do |f|
+      @fish[f.age] += 1
+    end
   end
 
   def step
-    new_fish = []
-    @fish.each do |fish|
-      if fish.increment
-        new_fish << Fish.new(8)
-      end
+    fish = Array.new(9, 0)
+    @fish = @fish.each_with_index do |count, age|
+      f = Fish.new(age)
+      fish[8] += count if f.increment
+      fish[f.age] += count
     end
 
-    @fish = @fish + new_fish
+    @fish = fish
   end
 
   def size
-    @fish.size
+    @fish.sum
   end
 end
 
 class Fish
-  attr_reader :day
-  def initialize(day)
-    @day = day
+  attr_reader :age
+  def initialize(age)
+    @age = age
   end
 
   def increment
-    if @day > 0
-      @day -= 1
-      return false
+    new_fish = false
+
+    if @age > 0
+      @age -= 1
     else
-      @day = 6
-      return true
+      @age = 6
+      new_fish = true
     end
+
+    new_fish
   end
 end
 
