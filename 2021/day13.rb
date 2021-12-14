152f477a70a74314b53d8d3d943451190c0b817f
Mike Skalnik <mike@skalnik.com>
2021-12-13 16:49:24 -0800

    💄

--- 2021/day13.rb
+++ 2021/day13.rb
@@ -46,9 +46,9 @@ class Day13
     (0..max_x).each do |x|
       (0..max_y).each do |y|
         if points.include? [x, y]
-          print "#"
+          print "❎"
         else
-          print " "
+          print "🏴"
         end
       end
 
